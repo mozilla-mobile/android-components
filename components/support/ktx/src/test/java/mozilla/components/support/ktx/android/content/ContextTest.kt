da42a645ebdfc334bb38aec2b9f6903e7d37b875
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2018-09-15 10:36:55 +0530

    isScreenReaderEnabled: add Roboelectric tests

--- components/support/ktx/src/test/java/mozilla/components/support/ktx/android/content/ContextTest.kt
+++ components/support/ktx/src/test/java/mozilla/components/support/ktx/android/content/ContextTest.kt
@@ -6,29 +6,42 @@ package mozilla.components.support.ktx.android.content
 
 import android.app.ActivityManager
 import android.content.Context
-import org.junit.Assert.assertEquals
+import android.view.accessibility.AccessibilityManager
+import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
+import org.robolectric.Shadows.shadowOf
 
 @RunWith(RobolectricTestRunner::class)
 class ContextTest {
+
+    private var context: Context? = null
+    private var accessibilityManager: AccessibilityManager? = null
+
+    @Before
+    fun startUp() {
+        context = RuntimeEnvironment.application
+        accessibilityManager = context!!.getAccessibilityManager()
+    }
+
     @Test
     fun `systemService() returns same service as getSystemService()`() {
         val context = RuntimeEnvironment.application
 
         assertEquals(
-            context.getSystemService(Context.INPUT_METHOD_SERVICE),
-            context.systemService(Context.INPUT_METHOD_SERVICE))
+                context.getSystemService(Context.INPUT_METHOD_SERVICE),
+                context.systemService(Context.INPUT_METHOD_SERVICE))
 
         assertEquals(
-            context.getSystemService(Context.ACTIVITY_SERVICE),
-            context.systemService(Context.ACTIVITY_SERVICE))
+                context.getSystemService(Context.ACTIVITY_SERVICE),
+                context.systemService(Context.ACTIVITY_SERVICE))
 
         assertEquals(
-            context.getSystemService(Context.LOCATION_SERVICE),
-            context.systemService(Context.LOCATION_SERVICE))
+                context.getSystemService(Context.LOCATION_SERVICE),
+                context.systemService(Context.LOCATION_SERVICE))
     }
 
     @Test
@@ -43,5 +56,17 @@ class ContextTest {
         }.lowMemory
 
         assertEquals(extensionFunctionResult, normalMethodResult)
+    }
+
+    @Test
+    fun `isScreenReaderEnabled() returns true when TouchExploration enabled`() {
+        shadowOf(accessibilityManager).isTouchExplorationEnabled = true
+        assertTrue(context!!.isScreenReaderEnabled())
+    }
+
+    @Test
+    fun `isScreenReaderEnabled() returns false when TouchExploration disabled`() {
+        shadowOf(accessibilityManager).isTouchExplorationEnabled = false
+        assertFalse(context!!.isScreenReaderEnabled())
     }
 }

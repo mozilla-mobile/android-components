42fbf9eb4b0ca55c63f58ec454ba2eac90341266
Arturo Mejia <arturomejiamarmol@gmail.com>
2018-09-30 00:08:56 -0400

    New_GK_Nightly_Version

--- buildSrc/src/main/java/GeckoVersions.kt
+++ buildSrc/src/main/java/GeckoVersions.kt
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
+ 
 object GeckoVersions {
-    const val nightly_version = "64.0.20180927100037"
+    const val nightly_version = "64.0.20180928100055"
 }

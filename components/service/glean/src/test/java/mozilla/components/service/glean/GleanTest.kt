0b34ef3fcb814056bfb095c7f65e257c9f113c7c
Michael Droettboom <mdboom@gmail.com>
2019-03-19 12:04:25 -0400

    Fix don't schedule pings test

--- components/service/glean/src/test/java/mozilla/components/service/glean/GleanTest.kt
+++ components/service/glean/src/test/java/mozilla/components/service/glean/GleanTest.kt
@@ -262,6 +262,8 @@ class GleanTest {
             Glean.handleBackgroundEvent()
         }
 
+        triggerWorkManager()
+
         // We should only have a baseline ping and no events or metrics pings since nothing was
         // recorded
         val files = Glean.pingStorageEngine.storageDirectory.listFiles()

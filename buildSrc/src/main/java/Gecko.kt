1440198af02e64d48ab53437318df786b68b0850
MickeyMoz <sebastian@mozilla.com>
2019-06-21 14:13:32 +0000

    Update GeckoView (nightly) (20190621-141330)

--- buildSrc/src/main/java/Gecko.kt
+++ buildSrc/src/main/java/Gecko.kt
@@ -6,7 +6,7 @@ internal object GeckoVersions {
     /**
      * GeckoView Nightly Version.
      */
-    const val nightly_version = "69.0.20190620094631"
+    const val nightly_version = "69.0.20190621093603"
 
     /**
      * GeckoView Beta Version.

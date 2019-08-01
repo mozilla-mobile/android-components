9f8d4d43b221bd2ad6fd7c36ffdba9d68d934d3e
Sebastian Kaspari <s.kaspari@gmail.com>
2019-08-01 13:30:56 +0200

    Rename MediaNotificationFeature to MediaFeature.

--- samples/browser/src/main/java/org/mozilla/samples/browser/DefaultComponents.kt
+++ samples/browser/src/main/java/org/mozilla/samples/browser/DefaultComponents.kt
@@ -29,8 +29,8 @@ import mozilla.components.concept.engine.DefaultSettings
 import mozilla.components.concept.engine.Engine
 import mozilla.components.feature.customtabs.CustomTabIntentProcessor
 import mozilla.components.feature.intent.TabIntentProcessor
+import mozilla.components.feature.media.MediaFeature
 import mozilla.components.feature.media.RecordingDevicesNotificationFeature
-import mozilla.components.feature.media.notification.MediaNotificationFeature
 import mozilla.components.feature.media.state.MediaStateMachine
 import mozilla.components.feature.pwa.ManifestStorage
 import mozilla.components.feature.pwa.intent.WebAppIntentProcessor
@@ -90,7 +90,7 @@ open class DefaultComponents(private val applicationContext: Context) {
             RecordingDevicesNotificationFeature(applicationContext, sessionManager = this)
                 .enable()
 
-            MediaNotificationFeature(applicationContext)
+            MediaFeature(applicationContext)
                 .enable()
 
             MediaStateMachine.start(this)

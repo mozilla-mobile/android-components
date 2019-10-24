6881d75011d333ade840f769f199078e77fc9aa0
Gaurav <kondharegaurav@gmail.com>
2019-10-24 21:32:04 +0530

    #4261 Updated the graphic on qr code scanning screen
    - Added an overlay that allows dimming the background and the reticle comes into focus.
    - Removed the previous reticle png images.

---
+++ components/feature/qr/src/main/java/mozilla/components/feature/qr/CustomOverlay.kt
@@ -0,0 +1,93 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package mozilla.components.feature.qr
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffXfermode
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.util.DisplayMetrics
+import android.widget.ImageView
+import android.widget.LinearLayout
+
+/**
+ * A custom overlay [View] that sets the opacity of the background to dim it.
+ * This gives a focus like look to the qr reticle.
+ * @property windowFrame a reference to a [Bitmap], used to create the window frame.
+ * @property activity a reference to an [Activity], used to refer the child views of
+ * calling parent view.
+ *
+ */
+class CustomOverlay : LinearLayout {
+    private var windowFrame: Bitmap? = null
+    private lateinit var activity: Activity
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        activity = context as Activity
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    /**
+     * This is called before the canvas is drawn. Initializes the windowFrame only once
+     * @param canvas The canvas to draw upon.
+     */
+    override fun dispatchDraw(canvas: Canvas) {
+        super.dispatchDraw(canvas)
+
+        if (windowFrame == null) {
+            createWindowFrame()
+        }
+        canvas.drawBitmap(windowFrame!!, 0f, 0f, null)
+    }
+
+    override fun isEnabled(): Boolean { return false }
+
+    override fun isClickable(): Boolean { return false }
+
+    /**
+     * This function creates the windows frame around our R.id.qr_reticle ImageView
+     * dynamically, so this will scale for all display sizes. Sets an alpha of 170%
+     * to give a dimmed look for the background
+     */
+    private fun createWindowFrame() {
+        val dm = DisplayMetrics()
+        val imageView = activity.findViewById(R.id.qr_reticle) as ImageView
+        val loc = IntArray(2)
+        imageView.getLocationInWindow(loc)
+        activity.windowManager.defaultDisplay.getMetrics(dm)
+        val topOffset = dm.heightPixels - height
+        val bottom = imageView.bottom
+        val right = imageView.right
+
+        windowFrame = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val osCanvas = Canvas(windowFrame!!)
+
+        val outerRectangle = RectF(0f, 0f, width.toFloat(), height.toFloat())
+
+        var paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        paint.color = Color.argb(170, 0, 0, 0)
+        osCanvas.drawRect(outerRectangle, paint)
+
+        paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_OUT)
+        val innerRectangle = RectF(loc[0].toFloat(), (loc[1] - topOffset).toFloat(), (width - (width - right)).toFloat(), (height - (height - bottom)).toFloat())
+        osCanvas.drawRoundRect(innerRectangle, 30.0f, 30.0f, paint)
+    }
+
+    override fun isInEditMode(): Boolean { return true }
+
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        super.onLayout(changed, l, t, r, b)
+        windowFrame = null
+    }
+}

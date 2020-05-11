c16d0b6b4bfde29ad92fe03007aab81ecc3f4dda
Sebastian Kaspari <s.kaspari@gmail.com>
2020-05-11 10:39:00 +0200

    Update Kotlin to 1.3.72.

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -4,7 +4,7 @@
 
 // Synchronized version numbers for dependencies used by (some) modules
 object Versions {
-    const val kotlin = "1.3.71"
+    const val kotlin = "1.3.72"
     const val coroutines = "1.3.6"
 
     const val junit = "4.12"

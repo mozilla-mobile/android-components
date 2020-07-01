0892d1b53a93a162513292d68f960c51b271b8fb
Sebastian Kaspari <s.kaspari@gmail.com>
2020-07-01 11:13:13 +0200

    Switch back to GeckoView Beta 79.0.20200629161151 (optimized build).

--- buildSrc/src/main/java/Gecko.kt
+++ buildSrc/src/main/java/Gecko.kt
@@ -11,7 +11,7 @@ internal object GeckoVersions {
     /**
      * GeckoView Beta Version.
      */
-    const val beta_version = "79.0.20200629163142"
+    const val beta_version = "79.0.20200629161151"
 
     /**
      * GeckoView Release Version.

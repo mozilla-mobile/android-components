e4daf16a5d478186c5c5313f2e75f6edb8b56409
MickeyMoz <sebastian@mozilla.com>
2020-07-04 14:05:16 +0000

    Update GeckoView (nightly) (20200704-140516)

--- buildSrc/src/main/java/Gecko.kt
+++ buildSrc/src/main/java/Gecko.kt
@@ -6,7 +6,7 @@ internal object GeckoVersions {
     /**
      * GeckoView Nightly Version.
      */
-    const val nightly_version = "80.0.20200703094420"
+    const val nightly_version = "80.0.20200704091658"
 
     /**
      * GeckoView Beta Version.

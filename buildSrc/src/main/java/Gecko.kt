4941d73cd5972cc717f41734770e1c77304b488f
Stefan Arentz <stefan@arentz.ca>
2020-10-27 09:19:24 -0400

    Update Geckoview (Beta) to 83.0.20201025174155

--- buildSrc/src/main/java/Gecko.kt
+++ buildSrc/src/main/java/Gecko.kt
@@ -11,7 +11,7 @@ internal object GeckoVersions {
     /**
      * GeckoView Beta Version.
      */
-    const val beta_version = "83.0.20201019162914"
+    const val beta_version = "83.0.20201025174155"
 
     /**
      * GeckoView Release Version.

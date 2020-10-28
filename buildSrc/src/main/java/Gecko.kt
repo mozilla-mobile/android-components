d81e7fb3b4047ce1633e4c0b681982b9c2540ce2
st3fan <stefan@arentz.ca>
2020-10-28 09:17:27 -0400

    Update GeckoView (Release) 82.0.20201027185343.

--- buildSrc/src/main/java/Gecko.kt
+++ buildSrc/src/main/java/Gecko.kt
@@ -16,7 +16,7 @@ internal object GeckoVersions {
     /**
      * GeckoView Release Version.
      */
-    const val release_version = "82.0.20201014125134"
+    const val release_version = "82.0.20201027185343"
 }
 
 @Suppress("Unused", "MaxLineLength")

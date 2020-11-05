2044d4fad79b05c33ef899f3e94a244d895341f7
Sebastian Kaspari <s.kaspari@gmail.com>
2020-11-05 14:14:05 +0100

    Test: Bump Glean to 33.1.2.

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -32,7 +32,7 @@ object Versions {
 
     const val mozilla_appservices = "63.0.0"
 
-    const val mozilla_glean = "33.0.4"
+    const val mozilla_glean = "33.1.2"
 
     const val material = "1.1.0"
     const val nearby = "17.0.0"

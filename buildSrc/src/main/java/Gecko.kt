24dd57cfd2d2aeeb58a39f59c127b1893c57eaa3
MickeyMoz <sebastian@mozilla.com>
2020-12-12 10:51:38 +0000

    Update GeckoView (Release) to 84.0.20201211215739.

--- buildSrc/src/main/java/Gecko.kt
+++ buildSrc/src/main/java/Gecko.kt
@@ -16,7 +16,7 @@ internal object GeckoVersions {
     /**
      * GeckoView Release Version.
      */
-    const val release_version = "84.0.20201210144358"
+    const val release_version = "84.0.20201211215739"
 }
 
 @Suppress("Unused", "MaxLineLength")

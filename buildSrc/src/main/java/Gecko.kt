313d434990f0e0abc1dbea8e3b557f36cacfb069
MickeyMoz <sebastian@mozilla.com>
2021-02-22 22:10:48 +0000

    Update GeckoView (Release) to 86.0.20210222142601.

--- buildSrc/src/main/java/Gecko.kt
+++ buildSrc/src/main/java/Gecko.kt
@@ -16,7 +16,7 @@ internal object GeckoVersions {
     /**
      * GeckoView Release Version.
      */
-    const val release_version = "86.0.20210217195321"
+    const val release_version = "86.0.20210222142601"
 }
 
 @Suppress("Unused", "MaxLineLength")

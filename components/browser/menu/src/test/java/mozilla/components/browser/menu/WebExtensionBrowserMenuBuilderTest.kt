cabc59eeb19455e414b07b030a751782fc38a762
mcarare <mihai.carare.dev@gmail.com>
2021-04-16 12:51:12 +0300

    For #10092: Allow placeholder item to change tint for the extension icon.

--- components/browser/menu/src/test/java/mozilla/components/browser/menu/WebExtensionBrowserMenuBuilderTest.kt
+++ components/browser/menu/src/test/java/mozilla/components/browser/menu/WebExtensionBrowserMenuBuilderTest.kt
@@ -210,6 +210,7 @@ class WebExtensionBrowserMenuBuilderTest {
     fun `web extension is moved to main menu when extension id equals WebExtensionPlaceholderMenuItem id`() {
         val promotableWebExtensionId = "promotable extension id"
         val promotableWebExtensionTitle = "promotable extension action title"
+        val testIconTintColorResource = R.color.accent_material_dark
 
         val pageAction = WebExtensionBrowserAction("page_action", true, mock(), "", 0, 0) {}
         val pageActionPromotableWebExtension = WebExtensionBrowserAction(promotableWebExtensionTitle, true, mock(), "", 0, 0) {}
@@ -241,7 +242,10 @@ class WebExtensionBrowserMenuBuilderTest {
 
         // 4 items initially on the main menu
         val items = listOf(
-            WebExtensionPlaceholderMenuItem(id = promotableWebExtensionId),
+            WebExtensionPlaceholderMenuItem(
+                id = promotableWebExtensionId,
+                iconTintColorResource = testIconTintColorResource
+            ),
             mockMenuItem(),
             submenuPlaceholderMenuItem,
             mockMenuItem())
@@ -272,6 +276,9 @@ class WebExtensionBrowserMenuBuilderTest {
 
         // the replaced item should have the action title of the WebExtensionBrowserMenuItem
         assertEquals(promotableWebExtensionTitle, (replacedItem as WebExtensionBrowserMenuItem).action.title)
+
+        // the replaced item should have the icon tint set by placeholder
+        assertEquals(testIconTintColorResource, replacedItem.iconTintColorResource)
 
         val parentMenuItem = menu.adapter.visibleItems[2] as? ParentBrowserMenuItem
         val subMenuItemSize = parentMenuItem!!.subMenu.adapter.visibleItems.size

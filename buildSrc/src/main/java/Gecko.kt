2b8d1172450fcd448255de45f53fc7a1641a35f7
MickeyMoz <sebastian@mozilla.com>
2021-05-20 21:37:03 +0000

    Update GeckoView (Beta) to 89.0.20210520185752.

--- buildSrc/src/main/java/Gecko.kt
+++ buildSrc/src/main/java/Gecko.kt
@@ -11,7 +11,7 @@ internal object GeckoVersions {
     /**
      * GeckoView Beta Version.
      */
-    const val beta_version = "89.0.20210518190425"
+    const val beta_version = "89.0.20210520185752"
 
     /**
      * GeckoView Release Version.

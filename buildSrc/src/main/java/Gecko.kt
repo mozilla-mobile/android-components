b92d233568f21ef48968458e3b8d19d21e4c0354
Sebastian Kaspari <s.kaspari@gmail.com>
2021-05-25 15:42:54 +0200

    Update to GeckoView 89 RC.

--- buildSrc/src/main/java/Gecko.kt
+++ buildSrc/src/main/java/Gecko.kt
@@ -16,7 +16,7 @@ internal object GeckoVersions {
     /**
      * GeckoView Release Version.
      */
-    const val release_version = "88.0.20210504152106"
+    const val release_version = "89.0.20210524222230"
 }
 
 @Suppress("Unused", "MaxLineLength")

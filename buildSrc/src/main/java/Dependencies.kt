0e8999bc032470b21a4c9e3e71cd34d3f22e53e7
Sebastian Kaspari <s.kaspari@gmail.com>
2021-06-01 11:29:13 +0200

    Update to Robolectric 4.5.1.

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -11,7 +11,7 @@ object Versions {
     const val coroutines = "1.4.2"
 
     const val junit = "4.12"
-    const val robolectric = "4.1"
+    const val robolectric = "4.5.1"
     const val mockito = "2.24.5"
     const val maven_ant_tasks = "2.1.3"
 

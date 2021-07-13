22a1b284caa0727a16f74bed9f49db558ad011dd
Christian Sadilek <christian.sadilek@gmail.com>
2021-07-13 10:38:13 -0400

    Update to GeckoView 91.0.20210712122804 (Beta)

--- buildSrc/src/main/java/Gecko.kt
+++ buildSrc/src/main/java/Gecko.kt
@@ -9,12 +9,12 @@ object Gecko {
     /**
      * GeckoView Version.
      */
-    const val version = "91.0.20210712093040"
+    const val version = "91.0.20210712122804"
 
     /**
      * GeckoView channel
      */
-    val channel = GeckoChannel.NIGHTLY
+    val channel = GeckoChannel.BETA
 }
 
 /**

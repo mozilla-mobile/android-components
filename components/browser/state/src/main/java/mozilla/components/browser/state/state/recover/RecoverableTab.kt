mozilla-mobile/android-components

commit 5c6977d4d57224e8301a6872b253d7848118c351

Introduced 'restored' content state for tracking tab restoration

components/browser/state/src/main/java/mozilla/components/browser/state/state/recover/RecoverableTab.kt
@@ -47,7 +47,7 @@ data class RecoverableTab(
     val lastMediaAccessState: LastMediaAccessState = LastMediaAccessState(),
     val private: Boolean = false,
     val historyMetadata: HistoryMetadataKey? = null,
-    val source: Source = Source.Internal.Restored
+    val source: Source = Source.Internal.None
 )
 
 /**
@@ -84,10 +84,11 @@ fun RecoverableTab.toTabSessionState() = createTab(
     createdAt = createdAt,
     lastMediaAccessState = lastMediaAccessState,
     historyMetadata = historyMetadata,
-    source = source
+    source = source,
+    restored = true
 )
 
 /**
- * Creates a list of [TabSessionState]s from a List of [TabSessionState]s.
+ * Creates a list of [TabSessionState]s from a List of [RecoverableTab]s.
  */
 fun List<RecoverableTab>.toTabSessionStates() = map { it.toTabSessionState() }

components/browser/session-storage/src/main/java/mozilla/components/browser/session/storage/serialize/BrowserStateReader.kt
@@ -181,7 +181,7 @@ private fun JsonReader.tabSession(): RecoverableTab {
     var historyMetadataSearchTerm: String? = null
     var historyMetadataReferrerUrl: String? = null
 
-    var externalSourceId: Int? = null
+    var sourceId: Int? = null
     var externalSourcePackageId: String? = null
     var externalSourceCategory: Int? = null
 
@@ -204,10 +204,10 @@ private fun JsonReader.tabSession(): RecoverableTab {
             Keys.SESSION_LAST_MEDIA_URL -> lastMediaUrl = nextString()
             Keys.SESSION_LAST_MEDIA_SESSION_ACTIVE -> mediaSessionActive = nextBoolean()
             Keys.SESSION_LAST_MEDIA_ACCESS -> lastMediaAccess = nextLong()
-            Keys.SESSION_EXTERNAL_SOURCE_ID -> externalSourceId = nextInt()
+            Keys.SESSION_SOURCE_ID -> sourceId = nextInt()
             Keys.SESSION_EXTERNAL_SOURCE_PACKAGE_ID -> externalSourcePackageId = nextStringOrNull()
             Keys.SESSION_EXTERNAL_SOURCE_PACKAGE_CATEGORY -> externalSourceCategory = nextIntOrNull()
-            Keys.SESSION_SOURCE_KEY -> nextString()
+            Keys.SESSION_DEPRECATED_SOURCE_KEY -> nextString()
             else -> throw IllegalArgumentException("Unknown session key: $name")
         }
     }
@@ -242,6 +242,6 @@ private fun JsonReader.tabSession(): RecoverableTab {
             lastMediaAccess = lastMediaAccess ?: 0,
             mediaSessionActive = mediaSessionActive ?: false
         ),
-        source = SessionState.Source.restore(externalSourceId, externalSourcePackageId, externalSourceCategory)
+        source = SessionState.Source.restore(sourceId, externalSourcePackageId, externalSourceCategory)
     )
 }

components/browser/session-storage/src/main/java/mozilla/components/browser/session/storage/serialize/BrowserStateWriter.kt
@@ -121,11 +121,11 @@ private fun JsonWriter.tab(
             value(metadata.referrerUrl)
         }
 
-        (tab.source as? SessionState.Source.External)?.let { source ->
-            name(Keys.SESSION_EXTERNAL_SOURCE_ID)
-            value(source.id)
+        name(Keys.SESSION_SOURCE_ID)
+        value(tab.source.id)
 
-            source.caller?.let { caller ->
+        (tab.source as? SessionState.Source.External)?.let { externalSource ->
+            externalSource.caller?.let { caller ->
                 name(Keys.SESSION_EXTERNAL_SOURCE_PACKAGE_ID)
                 value(caller.packageId)
 

components/browser/session-storage/src/main/java/mozilla/components/browser/session/storage/serialize/Keys.kt
@@ -26,13 +26,14 @@ internal object Keys {
     const val SESSION_LAST_MEDIA_URL = "lastMediaUrl"
     const val SESSION_LAST_MEDIA_ACCESS = "lastMediaAccess"
     const val SESSION_LAST_MEDIA_SESSION_ACTIVE = "mediaSessionActive"
-    const val SESSION_SOURCE_KEY = "source"
+    // Deprecated for SESSION_SOURCE_ID, kept around for backwards compatibility.
+    const val SESSION_DEPRECATED_SOURCE_KEY = "source"
 
     const val SESSION_HISTORY_METADATA_URL = "historyMetadataUrl"
     const val SESSION_HISTORY_METADATA_SEARCH_TERM = "historyMetadataSearchTerm"
     const val SESSION_HISTORY_METADATA_REFERRER_URL = "historyMetadataReferrerUrl"
 
-    const val SESSION_EXTERNAL_SOURCE_ID = "externalSourceId"
+    const val SESSION_SOURCE_ID = "sourceId"
     const val SESSION_EXTERNAL_SOURCE_PACKAGE_ID = "externalPackageId"
     const val SESSION_EXTERNAL_SOURCE_PACKAGE_CATEGORY = "externalPackageCategory"
 

components/browser/session-storage/src/test/java/mozilla/components/browser/session/storage/serialize/BrowserStateWriterReaderTest.kt
@@ -97,7 +97,7 @@ class BrowserStateWriterReaderTest {
     }
 
     @Test
-    fun `Read tab with session source`() {
+    fun `Read tab with deprecated session source`() {
         // We don't persist session source of tabs unless it's an external source.
         // However, in older versions we did persist other types of sources so need to be tolerant
         // if these older cases are encountered in JSON to remain backward compatible.
@@ -107,14 +107,14 @@ class BrowserStateWriterReaderTest {
         val file = AtomicFile(
             File.createTempFile(UUID.randomUUID().toString(), UUID.randomUUID().toString())
         )
-        writeTabWithSource(tab, file)
+        writeTabWithDeprecatedSource(tab, file)
 
-        // When reading a tab that didn't have an external source persisted, we just need to make sure
+        // When reading a tab that didn't have a source persisted, we just need to make sure
         // it is deserialized correctly. In this case, source defaults to `Internal.Restored`.
         val reader = BrowserStateReader()
         val restoredTab = reader.readTab(engine, file)
         assertNotNull(restoredTab!!)
-        assertEquals(SessionState.Source.Internal.Restored, restoredTab.source)
+        assertEquals(SessionState.Source.Internal.None, restoredTab.source)
 
         assertEquals("https://www.mozilla.org", restoredTab.url)
         assertEquals("Mozilla", restoredTab.title)
@@ -356,11 +356,11 @@ private fun createFakeEngine(engineState: EngineSessionState): Engine {
     return engine
 }
 
-private fun writeTabWithSource(tab: TabSessionState, file: AtomicFile) {
-    file.streamJSON { tabWithSource(tab) }
+private fun writeTabWithDeprecatedSource(tab: TabSessionState, file: AtomicFile) {
+    file.streamJSON { tabWithDeprecatedSource(tab) }
 }
 
-private fun JsonWriter.tabWithSource(
+private fun JsonWriter.tabWithDeprecatedSource(
     tab: TabSessionState
 ) {
     beginObject()
@@ -376,7 +376,7 @@ private fun JsonWriter.tabWithSource(
         name(Keys.SESSION_TITLE)
         value(tab.content.title)
 
-        name(Keys.SESSION_SOURCE_KEY)
+        name(Keys.SESSION_DEPRECATED_SOURCE_KEY)
         value(tab.source.toString())
 
         endObject()

components/browser/state/src/main/java/mozilla/components/browser/state/state/CustomTabSessionState.kt
@@ -30,7 +30,8 @@ data class CustomTabSessionState(
     override val extensionState: Map<String, WebExtensionState> = emptyMap(),
     override val mediaSessionState: MediaSessionState? = null,
     override val contextId: String? = null,
-    override val source: SessionState.Source = SessionState.Source.Internal.CustomTab
+    override val source: SessionState.Source = SessionState.Source.Internal.CustomTab,
+    override val restored: Boolean = false
 ) : SessionState {
 
     override fun createCopy(

components/browser/state/src/main/java/mozilla/components/browser/state/state/SessionState.kt
@@ -21,7 +21,7 @@ import mozilla.components.support.utils.SafeIntent
  * @property contextId the session context ID of the session. The session context ID specifies the
  * contextual identity to use for the session's cookie store.
  * https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Work_with_contextual_identities
- * @property source the [Source] of this session to describe how and why it was created.
+ * @property restored Indicates if this session was restored from a hydrated state.
  */
 interface SessionState {
     val id: String
@@ -32,6 +32,7 @@ interface SessionState {
     val mediaSessionState: MediaSessionState?
     val contextId: String?
     val source: Source
+    val restored: Boolean
 
     /**
      * Copy the class and override some parameters.
@@ -69,9 +70,15 @@ interface SessionState {
                     2 -> External.ActionView(caller)
                     3 -> External.ActionSearch(caller)
                     4 -> External.CustomTab(caller)
-                    // We only care about restoring 'external' types, so collapse other source types.
-                    // This also silently handles abnormalities (like unknown or null sourceId).
-                    else -> Internal.Restored
+                    5 -> Internal.HomeScreen
+                    6 -> Internal.Menu
+                    7 -> Internal.NewTab
+                    8 -> Internal.None
+                    9 -> Internal.TextSelection
+                    10 -> Internal.UserEntered
+                    11 -> Internal.CustomTab
+                    // Silently handle abnormalities (like invalid or null sourceId).
+                    else -> Internal.None
                 }
             }
         }
@@ -136,14 +143,9 @@ interface SessionState {
             object UserEntered : Internal(10)
 
             /**
-             * This session was restored.
-             */
-            object Restored : Internal(11)
-
-            /**
              * Created to handle a CustomTabs intent of internal origin.
              */
-            object CustomTab : Internal(12)
+            object CustomTab : Internal(11)
         }
     }
 }
@@ -167,19 +169,7 @@ enum class PackageCategory(val id: Int) {
         /**
          * Maps an int category (as it can be obtained from a package manager) to our internal representation.
          */
-        fun fromInt(id: Int?): PackageCategory = when (id) {
-            0 -> GAME
-            1 -> AUDIO
-            2 -> VIDEO
-            3 -> IMAGE
-            4 -> SOCIAL
-            5 -> NEWS
-            6 -> MAPS
-            7 -> PRODUCTIVITY
-            -1 -> UNKNOWN
-            null -> UNKNOWN
-            else -> UNKNOWN
-        }
+        fun fromInt(id: Int?): PackageCategory = values().find { category -> category.id == id } ?: UNKNOWN
     }
 }
 

components/browser/state/src/main/java/mozilla/components/browser/state/state/TabSessionState.kt
@@ -29,6 +29,7 @@ import java.util.UUID
  * @property createdAt Timestamp of this tab's creation.
  * @property lastMediaAccessState - [LastMediaAccessState] detailing the tab state when media started playing.
  * Requires [LastMediaAccessMiddleware] to update the value when playback starts.
+ * @property restored Indicates if this page was restored from a persisted state.
  */
 data class TabSessionState(
     override val id: String = UUID.randomUUID().toString(),
@@ -39,6 +40,7 @@ data class TabSessionState(
     override val mediaSessionState: MediaSessionState? = null,
     override val contextId: String? = null,
     override val source: SessionState.Source = SessionState.Source.Internal.None,
+    override val restored: Boolean = false,
     val parentId: String? = null,
     val lastAccess: Long = 0L,
     val createdAt: Long = System.currentTimeMillis(),
@@ -85,6 +87,7 @@ fun createTab(
     createdAt: Long = System.currentTimeMillis(),
     lastMediaAccessState: LastMediaAccessState = LastMediaAccessState(),
     source: SessionState.Source = SessionState.Source.Internal.None,
+    restored: Boolean = false,
     engineSession: EngineSession? = null,
     engineSessionState: EngineSessionState? = null,
     crashed: Boolean = false,
@@ -112,6 +115,7 @@ fun createTab(
         createdAt = createdAt,
         lastMediaAccessState = lastMediaAccessState,
         source = source,
+        restored = restored,
         engineState = EngineState(
             engineSession = engineSession,
             engineSessionState = engineSessionState,

commit 7cd5b059458a987eba4abb307c4b136f8ee57d47

Make sure to preserve private state when recreating a RecoverableTab
regression from https://github.com/mozilla-mobile/android-components/pull/10711/files

components/browser/state/src/main/java/mozilla/components/browser/state/state/recover/RecoverableTab.kt
@@ -83,6 +83,7 @@ fun RecoverableTab.toTabSessionState() = createTab(
     lastAccess = lastAccess,
     createdAt = createdAt,
     lastMediaAccessState = lastMediaAccessState,
+    private = private,
     historyMetadata = historyMetadata,
     source = source,
     restored = true

components/browser/session-storage/src/test/java/mozilla/components/browser/session/storage/SessionStorageTest.kt
@@ -421,6 +421,7 @@ internal fun TabSessionState.assertSameAs(tab: RecoverableTab) {
     assertEquals(contextId, tab.contextId)
     assertEquals(lastAccess, tab.lastAccess)
     assertEquals(readerState, tab.readerState)
+    assertEquals(content.private, tab.private)
     assertEquals(
         (engineState.engineSessionState as? FakeEngineSessionState)?.value ?: "---",
         (tab.state as FakeEngineSessionState).value
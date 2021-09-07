3c65e00f8eb57b437a71e4f61d39bdcbf28694d4
Ben Dean-Kawamura <ben@mozilla.com>
2021-09-07 15:29:08 -0400

    Updating the sample browser to fake the new Logins API

--- samples/browser/src/main/java/org/mozilla/samples/browser/storage/DummyLoginsStorage.kt
+++ samples/browser/src/main/java/org/mozilla/samples/browser/storage/DummyLoginsStorage.kt
@@ -4,10 +4,70 @@
 
 package org.mozilla.samples.browser.storage
 
-import mozilla.components.concept.storage.Login
-import mozilla.components.concept.storage.LoginsStorage
+import mozilla.components.concept.storage.*
 import org.json.JSONObject
 import java.util.UUID
+
+// "Encryption" for us is just serializing to JSON
+internal fun encryptLoginData(username: String, password: String): String {
+    var obj = JSONObject()
+    obj.put("username", username)
+    obj.put("password", password)
+    return obj.toString()
+}
+
+internal fun decryptLoginData(secureFields: String): Pair<String, String> {
+    val obj = JSONObject(secureFields)
+    return Pair(obj.getString("username"), obj.getString("password"))
+}
+
+// Define some functions to convert between the login types
+
+fun LoginEntry.toNewEncryptedLogin() = EncryptedLogin(
+    guid = UUID.randomUUID().toString(),
+    origin = origin,
+    formActionOrigin = formActionOrigin,
+    httpRealm = httpRealm,
+    usernameField = usernameField,
+    passwordField = passwordField,
+    timesUsed = 0,
+    timeLastUsed = 0,
+    timeCreated = 0,
+    timePasswordChanged = 0,
+    secureFields =  encryptLoginData(username, password),
+)
+
+fun EncryptedLogin.toLogin(): Login {
+    val (username, password) = decryptLoginData(secureFields)
+    return Login(
+        guid = guid,
+        origin = origin,
+        formActionOrigin = formActionOrigin,
+        httpRealm = httpRealm,
+        usernameField = usernameField,
+        passwordField = passwordField,
+        username = username,
+        password = password,
+        timesUsed = timesUsed,
+        timeLastUsed = timeLastUsed,
+        timeCreated = timeCreated,
+        timePasswordChanged = timePasswordChanged,
+    )
+}
+
+fun EncryptedLogin.copyWithEntry(entry: LoginEntry) = EncryptedLogin(
+    guid = guid,
+    origin = entry.origin,
+    formActionOrigin = entry.formActionOrigin,
+    httpRealm = entry.httpRealm,
+    usernameField = entry.usernameField,
+    passwordField = entry.passwordField,
+    timesUsed = timesUsed,
+    timeLastUsed = timeLastUsed,
+    timeCreated = timeCreated,
+    timePasswordChanged = timePasswordChanged,
+    secureFields = encryptLoginData(entry.username, entry.password),
+)
 
 /**
  * A dummy [LoginsStorage] that returns a fixed set of fake logins. Currently sample browser does
@@ -16,59 +76,84 @@ import java.util.UUID
 class DummyLoginsStorage : LoginsStorage {
     // A list of fake logins for testing purposes.
     private val logins = mutableListOf(
-        Login(
+        EncryptedLogin(
             guid = "9282b6fd-97ba-4636-beca-975d9f4fd150",
-            origin = "twitter.com",
-            username = "NotAnAccount",
-            password = "NotReallyAPassword"
+            origin = "http://twitter.com/",
+            formActionOrigin = "http://twitter.com/",
+            httpRealm = null,
+            usernameField = "",
+            passwordField = "",
+            timesUsed = 0,
+            timeLastUsed = 0,
+            timeCreated = 0,
+            timePasswordChanged = 0,
+            secureFields =  encryptLoginData("NotAnAccount", "NotReallyAPassword"),
         ),
-        Login(
+        EncryptedLogin(
             guid = "8034a37f-5f9e-4136-95b2-e0293116b322",
-            origin = "twitter.com",
-            username = "",
-            password = "NotReallyAPassword"
+            origin = "http://twitter.com/",
+            formActionOrigin = "http://twitter.com/",
+            httpRealm = null,
+            usernameField = "",
+            passwordField = "",
+            timesUsed = 0,
+            timeLastUsed = 0,
+            timeCreated = 0,
+            timePasswordChanged = 0,
+            secureFields =  encryptLoginData("", "NotReallyAPassword"),
         )
     )
 
     override suspend fun touch(guid: String) = Unit
 
-    override suspend fun delete(id: String): Boolean = logins.removeAll { login -> login.guid == id }
+    override suspend fun delete(guid: String): Boolean = logins.removeAll { login -> login.guid == guid }
 
-    override suspend fun get(guid: String): Login? = logins.first { login -> login.guid == guid }
+    override suspend fun get(guid: String): EncryptedLogin? = logins.first { login -> login.guid == guid }
 
-    override suspend fun list(): List<Login> = logins
+    override suspend fun list(): List<EncryptedLogin> = logins
 
     override suspend fun wipe() = logins.clear()
 
     override suspend fun wipeLocal() = logins.clear()
 
-    override suspend fun add(login: Login): String {
-        val uuid = UUID.randomUUID().toString()
-        logins.add(login.copy(guid = uuid))
-        return uuid
-    }
-
-    override suspend fun update(login: Login) {
-        logins.map { current ->
-            if (current.guid == login.guid) {
-                login
-            } else {
-                current
-            }
+    override suspend fun add(entry: LoginEntry): EncryptedLogin {
+        return entry.toNewEncryptedLogin().also {
+            logins.add(it)
         }
     }
 
+    override suspend fun update(guid: String, entry: LoginEntry): EncryptedLogin {
+        val idx = logins.indexOfFirst { it.guid == guid }
+        logins[idx] = logins[idx].copyWithEntry(entry)
+        return logins[idx]
+    }
+
+    // Simplified version of the findLoginToUpdate logic from application-services
+    override fun findLoginToUpdate(entry: LoginEntry, logins: List<Login>): Login? {
+        return logins.firstOrNull { it.username == entry.username }
+    }
+
+    // Simplified version of the addOrUpdate logic from application-services
+    override suspend fun addOrUpdate(entry: LoginEntry): EncryptedLogin {
+        val existing = logins.firstOrNull {
+            it.origin == entry.origin && decryptLoginData(it.secureFields).first == entry.username
+        }
+        if (existing == null) {
+            return add(entry)
+        } else {
+            return update(existing.guid, entry)
+        }
+    }
+
+    override suspend fun getByBaseDomain(origin: String): List<EncryptedLogin> {
+        return logins.filter { it.origin == origin }
+    }
+
+    override suspend fun decryptLogins(logins: List<EncryptedLogin>): List<Login> {
+        return logins.map { it.toLogin() }
+    }
+
     override suspend fun importLoginsAsync(logins: List<Login>) = JSONObject()
-
-    override suspend fun ensureValid(login: Login) = Unit
-
-    override suspend fun getByBaseDomain(origin: String): List<Login> {
-        return logins.filter { login -> login.origin == origin }
-    }
-
-    override suspend fun getPotentialDupesIgnoringUsername(login: Login): List<Login> {
-        return emptyList()
-    }
 
     override fun close() = Unit
 }

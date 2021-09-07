b1cef4dee355a514af677e39467c03da75847e41
Sebastian Kaspari <s.kaspari@gmail.com>
2021-09-07 10:59:13 +0200

    Update to first GeckoView 93.0 Beta

--- buildSrc/src/main/java/Gecko.kt
+++ buildSrc/src/main/java/Gecko.kt
@@ -9,12 +9,12 @@ object Gecko {
     /**
      * GeckoView Version.
      */
-    const val version = "93.0.20210906031657"
+    const val version = "93.0.20210906164301"
 
     /**
      * GeckoView channel
      */
-    val channel = GeckoChannel.NIGHTLY
+    val channel = GeckoChannel.BETA
 }
 
 /**

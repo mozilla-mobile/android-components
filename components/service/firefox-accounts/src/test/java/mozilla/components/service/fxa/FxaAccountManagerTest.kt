3a96df65d25b46a3e401e65d17dbfa472773ab86
Ben Dean-Kawamura <ben@mozilla.com>
2021-09-09 16:20:57 -0400

    Test fixes

--- components/service/firefox-accounts/src/test/java/mozilla/components/service/fxa/FxaAccountManagerTest.kt
+++ components/service/firefox-accounts/src/test/java/mozilla/components/service/fxa/FxaAccountManagerTest.kt
@@ -794,7 +794,7 @@ class FxaAccountManagerTest {
     @Test
     fun `error reading persisted account`() = runBlocking {
         val accountStorage = mock<AccountStorage>()
-        val readException = FxaException("pretend we failed to parse the account")
+        val readException = FxaNetworkException("pretend we failed to fetch the account")
         `when`(accountStorage.read()).thenThrow(readException)
 
         val manager = TestableFxaAccountManager(

e65a07c7a43f81ff3f0651b29fd1c116695a6ea3
Grisha Kruglov <gkruglov@mozilla.com>
2021-10-13 02:05:43 -0700

    Add DisbandSearchGroupAction to allow removing search groups from tab state
    (cherry picked from commit 8f37c5da85d0d7276ce02a3c6616384cbb5bad79)

--- components/browser/state/src/main/java/mozilla/components/browser/state/action/BrowserAction.kt
+++ components/browser/state/src/main/java/mozilla/components/browser/state/action/BrowserAction.kt
@@ -1188,6 +1188,13 @@ sealed class HistoryMetadataAction : BrowserAction() {
         val tabId: String,
         val historyMetadataKey: HistoryMetadataKey
     ) : HistoryMetadataAction()
+
+    /**
+     * Removes [searchTerm] (and referrer) from any history metadata associated with tabs.
+     */
+    data class DisbandSearchGroupAction(
+        val searchTerm: String
+    ) : HistoryMetadataAction()
 }
 
 /**

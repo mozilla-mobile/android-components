8cba0c2ed6b150b3a952d55ec03262a8b45752bd
MickeyMoz <sebastian@mozilla.com>
2021-11-12 12:22:23 +0000

    Update GeckoView (Nightly) to 96.0.20211112092317.

--- buildSrc/src/main/java/Gecko.kt
+++ buildSrc/src/main/java/Gecko.kt
@@ -9,7 +9,7 @@ object Gecko {
     /**
      * GeckoView Version.
      */
-    const val version = "96.0.20211111045525"
+    const val version = "96.0.20211112092317"
 
     /**
      * GeckoView channel

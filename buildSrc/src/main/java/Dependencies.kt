5e2e1f608a8c331e159f89fcdc91ead23bf52374
MickeyMoz <sebastian@mozilla.com>
2022-01-18 19:00:24 +0000

    Update Glean to 42.1.0.

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -31,7 +31,7 @@ object Versions {
 
     const val mozilla_appservices = "86.2.2"
 
-    const val mozilla_glean = "42.0.1"
+    const val mozilla_glean = "42.1.0"
 
     const val material = "1.2.1"
 

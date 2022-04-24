64f338252d42aea6c7d1e38c2c92b9e1e845da09
MickeyMoz <sebastian@mozilla.com>
2022-04-24 12:41:48 +0000

    Update GeckoView (Nightly) to 101.0.20220424093858.

--- buildSrc/src/main/java/Gecko.kt
+++ buildSrc/src/main/java/Gecko.kt
@@ -9,7 +9,7 @@ object Gecko {
     /**
      * GeckoView Version.
      */
-    const val version = "101.0.20220423065829"
+    const val version = "101.0.20220424093858"
 
     /**
      * GeckoView channel

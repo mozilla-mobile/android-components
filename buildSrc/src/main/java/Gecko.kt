3e13bd5d189994e563bff634b0b32f14794fe26a
MickeyMoz <sebastian@mozilla.com>
2022-06-14 23:21:14 +0000

    Update GeckoView (Beta) to 102.0.20220614185842.

--- buildSrc/src/main/java/Gecko.kt
+++ buildSrc/src/main/java/Gecko.kt
@@ -9,7 +9,7 @@ object Gecko {
     /**
      * GeckoView Version.
      */
-    const val version = "102.0.20220612185901"
+    const val version = "102.0.20220614185842"
 
     /**
      * GeckoView channel

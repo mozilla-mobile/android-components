80dafaa716c058a36043d1e77bf5d3eccfd2e9b9
mcarare <mihai.carare.dev@gmail.com>
2022-07-01 12:38:55 +0300

    For #12378: Suppress queryBroadcastReceivers deprecation.

--- components/feature/pwa/src/test/java/mozilla/components/feature/pwa/WebAppShortcutManagerTest.kt
+++ components/feature/pwa/src/test/java/mozilla/components/feature/pwa/WebAppShortcutManagerTest.kt
@@ -89,6 +89,7 @@ class WebAppShortcutManagerTest {
             )
         )
         val session = buildInstallableSession(manifest)
+        @Suppress("DEPRECATION")
         `when`(packageManager.queryBroadcastReceivers(any(), anyInt())).thenReturn(emptyList())
 
         manager.requestPinShortcut(context, session)

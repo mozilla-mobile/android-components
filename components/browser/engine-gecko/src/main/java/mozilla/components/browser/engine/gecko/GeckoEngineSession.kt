6ba61ccd91bde25e91fc2fb9d8340cfe42c938e9
indu <indu@ramkystech.com>
2022-07-05 18:06:54 +0530

    Display the web content that triggered the app redirect
    Display the web content that triggered the app redirect, if the user navigate to the browser through the launcher icon or coming back from the app
    
    Co-Authored-By: Mugurell <Mugurell@users.noreply.github.com>

--- components/browser/engine-gecko/src/main/java/mozilla/components/browser/engine/gecko/GeckoEngineSession.kt
+++ components/browser/engine-gecko/src/main/java/mozilla/components/browser/engine/gecko/GeckoEngineSession.kt
@@ -96,6 +96,8 @@ class GeckoEngineSession(
     internal var currentUrl: String? = null
     internal var lastLoadRequestUri: String? = null
     internal var pageLoadingUrl: String? = null
+    internal var appRedirectUrl: String? = null
+    internal var isLastAppRedirectedUri: Boolean = false
     internal var scrollY: Int = 0
     // The Gecko site permissions for the loaded site.
     internal var geckoPermissions: List<ContentPermission> = emptyList()
@@ -473,6 +475,11 @@ class GeckoEngineSession(
                 return
             }
 
+            if (isLastAppRedirectedUri && url == appRedirectUrl) {
+                goBack(false)
+                return
+            }
+
             currentUrl = url
             initialLoad = false
             initialLoadRequest = null
@@ -589,6 +596,8 @@ class GeckoEngineSession(
                         is InterceptionResponse.Content -> loadData(data, mimeType, encoding)
                         is InterceptionResponse.Url -> loadUrl(url)
                         is InterceptionResponse.AppIntent -> {
+                            isLastAppRedirectedUri = true
+                            appRedirectUrl = lastLoadRequestUri
                             notifyObservers {
                                 onLaunchIntentRequest(url = url, appIntent = appIntent)
                             }
@@ -603,6 +612,8 @@ class GeckoEngineSession(
             }
 
             if (interceptionResponse !is InterceptionResponse.AppIntent) {
+                isLastAppRedirectedUri = false
+                appRedirectUrl = ""
                 lastLoadRequestUri = request.uri
             }
             return interceptionResponse
@@ -691,7 +702,9 @@ class GeckoEngineSession(
             // - non-top level visits (i.e. iframes).
             if (privateMode ||
                 (flags and GeckoSession.HistoryDelegate.VISIT_TOP_LEVEL) == 0 ||
-                (flags and GeckoSession.HistoryDelegate.VISIT_UNRECOVERABLE_ERROR) != 0
+                (flags and GeckoSession.HistoryDelegate.VISIT_UNRECOVERABLE_ERROR) != 0 ||
+                (isLastAppRedirectedUri && url == appRedirectUrl)
+
             ) {
                 return GeckoResult.fromValue(false)
             }
@@ -850,22 +863,24 @@ class GeckoEngineSession(
         }
 
         override fun onTitleChange(session: GeckoSession, title: String?) {
-            if (!privateMode) {
-                currentUrl?.let { url ->
-                    settings.historyTrackingDelegate?.let { delegate ->
-                        if (delegate.shouldStoreUri(url)) {
-                            // NB: There's no guarantee that the title change will be processed by the
-                            // delegate before the session is closed (and the corresponding coroutine
-                            // job is cancelled). Observers will always be notified of the title
-                            // change though.
-                            launch(coroutineContext) {
-                                delegate.onTitleChanged(url, title ?: "")
+            if (!isLastAppRedirectedUri) {
+                if (!privateMode) {
+                    currentUrl?.let { url ->
+                        settings.historyTrackingDelegate?.let { delegate ->
+                            if (delegate.shouldStoreUri(url)) {
+                                // NB: There's no guarantee that the title change will be processed by the
+                                // delegate before the session is closed (and the corresponding coroutine
+                                // job is cancelled). Observers will always be notified of the title
+                                // change though.
+                                launch(coroutineContext) {
+                                    delegate.onTitleChanged(url, title ?: "")
+                                }
                             }
                         }
                     }
                 }
+                notifyObservers { onTitleChange(title ?: "") }
             }
-            notifyObservers { onTitleChange(title ?: "") }
         }
 
         override fun onPreviewImage(session: GeckoSession, previewImageUrl: String) {

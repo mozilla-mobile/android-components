mozilla-mobile/android-components

commit 633c64fe2db3d6022713070424d38ba1dc0842bf

For #12443 - Add firefox suggest header for lib suggestions

components/compose/awesomebar/src/main/java/mozilla/components/compose/browser/awesomebar/AwesomeBar.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
@@ -41,10 +40,22 @@ fun AwesomeBar(
     onScroll: () -> Unit = {},
     profiler: Profiler? = null
 ) {
-    val groups = remember(providers) {
-        listOf(
+    val mapTitlesToProviders = HashMap<String?, ArrayList<AwesomeBar.SuggestionProvider>>()
+    for (provider in providers) {
+        var groupedProviders: ArrayList<AwesomeBar.SuggestionProvider>? =
+            mapTitlesToProviders[provider.groupTitle()]
+        if (groupedProviders == null) {
+            groupedProviders = ArrayList()
+        }
+        groupedProviders.add(provider)
+        mapTitlesToProviders[provider.groupTitle()] = groupedProviders
+    }
+    val groups: ArrayList<AwesomeBar.SuggestionProviderGroup> = ArrayList()
+    for (item in mapTitlesToProviders) {
+        groups.add(
             AwesomeBar.SuggestionProviderGroup(
-                providers = providers
+                providers = item.value,
+                title = item.key
             )
         )
     }

components/concept/awesomebar/src/main/java/mozilla/components/concept/awesomebar/AwesomeBar.kt
@@ -153,6 +153,11 @@ interface AwesomeBar {
         val id: String
 
         /**
+         * A header title for grouping the suggestions.
+         **/
+        fun groupTitle(): String? = null
+
+        /**
          * Fired when the user starts interacting with the awesome bar by entering text in the toolbar.
          *
          * The provider has the option to return an initial list of suggestions that will be displayed before the

components/compose/awesomebar/src/main/java/mozilla/components/compose/browser/awesomebar/AwesomeBarDefaults.kt
@@ -28,7 +28,7 @@ object AwesomeBarDefaults {
             alpha = ContentAlpha.medium
         ),
         autocompleteIcon: Color = MaterialTheme.colors.onSurface,
-        groupTitle: Color = MaterialTheme.colors.primary
+        groupTitle: Color = MaterialTheme.colors.onBackground,
     ) = AwesomeBarColors(
         background,
         title,

components/compose/awesomebar/src/main/java/mozilla/components/compose/browser/awesomebar/internal/SuggestionGroup.kt
@@ -4,11 +4,13 @@
 
 package mozilla.components.compose.browser.awesomebar.internal
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import mozilla.components.compose.browser.awesomebar.AwesomeBarColors
 
 /**
@@ -23,6 +25,11 @@ internal fun SuggestionGroup(
         title,
         color = colors.groupTitle,
         modifier = Modifier
-            .padding(4.dp)
+            .padding(
+                vertical = 24.dp,
+                horizontal = 16.dp
+            )
+            .fillMaxWidth(),
+        fontSize = 14.sp,
     )
 }

components/compose/awesomebar/src/main/java/mozilla/components/compose/browser/awesomebar/internal/Suggestions.kt
@@ -37,9 +37,11 @@ internal fun Suggestions(
     ) {
 
         suggestions.forEach { (group, suggestions) ->
-            group.title?.let { title ->
-                item(group.id) {
-                    SuggestionGroup(title, colors)
+            if (suggestions.isNotEmpty()) {
+                group.title?.let { title ->
+                    item(group.id) {
+                        SuggestionGroup(title, colors)
+                    }
                 }
             }
 

components/feature/awesomebar/src/main/java/mozilla/components/feature/awesomebar/provider/BookmarksStorageSuggestionProvider.kt
@@ -31,6 +31,7 @@ private const val BOOKMARKS_SUGGESTION_LIMIT = 20
  * @param engine optional [Engine] instance to call [Engine.speculativeConnect] for the
  * highest scored suggestion URL.
  * @param showEditSuggestion optional parameter to specify if the suggestion should show the edit button
+ * @param suggestionsHeader optional parameter to specify if the suggestion should have a header
  */
 class BookmarksStorageSuggestionProvider(
     private val bookmarksStorage: BookmarksStorage,
@@ -39,9 +40,13 @@ class BookmarksStorageSuggestionProvider(
     private val indicatorIcon: Drawable? = null,
     private val engine: Engine? = null,
     private val showEditSuggestion: Boolean = true,
+    private val suggestionsHeader: String? = null,
 ) : AwesomeBar.SuggestionProvider {
-
     override val id: String = UUID.randomUUID().toString()
+
+    override fun groupTitle(): String? {
+        return suggestionsHeader
+    }
 
     override suspend fun onInputChanged(text: String): List<AwesomeBar.Suggestion> {
         if (text.isEmpty()) {

components/feature/awesomebar/src/main/java/mozilla/components/feature/awesomebar/provider/CombinedHistorySuggestionProvider.kt
@@ -41,6 +41,7 @@ const val DEFAULT_COMBINED_SUGGESTION_LIMIT = 5
  * @param maxNumberOfSuggestions optional parameter to specify the maximum number of returned suggestions,
  * defaults to [DEFAULT_COMBINED_SUGGESTION_LIMIT].
  * @param showEditSuggestion optional parameter to specify if the suggestion should show the edit button
+ * @param suggestionsHeader optional parameter to specify if the suggestion should have a header
  */
 @Suppress("LongParameterList")
 class CombinedHistorySuggestionProvider(
@@ -51,8 +52,13 @@ class CombinedHistorySuggestionProvider(
     internal val engine: Engine? = null,
     @VisibleForTesting internal var maxNumberOfSuggestions: Int = DEFAULT_COMBINED_SUGGESTION_LIMIT,
     private val showEditSuggestion: Boolean = true,
+    private val suggestionsHeader: String? = null,
 ) : AwesomeBar.SuggestionProvider {
     override val id: String = UUID.randomUUID().toString()
+
+    override fun groupTitle(): String? {
+        return suggestionsHeader
+    }
 
     override suspend fun onInputChanged(text: String): List<AwesomeBar.Suggestion> = coroutineScope {
         if (text.isBlank()) {

components/feature/awesomebar/src/main/java/mozilla/components/feature/awesomebar/provider/HistoryMetadataSuggestionProvider.kt
@@ -35,6 +35,7 @@ const val DEFAULT_METADATA_SUGGESTION_LIMIT = 5
  * @param maxNumberOfSuggestions optional parameter to specify the maximum number of returned suggestions,
  * defaults to [DEFAULT_METADATA_SUGGESTION_LIMIT].
  * @param showEditSuggestion optional parameter to specify if the suggestion should show the edit button
+ * @param suggestionsHeader optional parameter to specify if the suggestion should have a header
  */
 class HistoryMetadataSuggestionProvider(
     private val historyStorage: HistoryMetadataStorage,
@@ -43,8 +44,13 @@ class HistoryMetadataSuggestionProvider(
     internal val engine: Engine? = null,
     @VisibleForTesting internal val maxNumberOfSuggestions: Int = DEFAULT_METADATA_SUGGESTION_LIMIT,
     private val showEditSuggestion: Boolean = true,
+    private val suggestionsHeader: String? = null,
 ) : AwesomeBar.SuggestionProvider {
     override val id: String = UUID.randomUUID().toString()
+
+    override fun groupTitle(): String? {
+        return suggestionsHeader
+    }
 
     override suspend fun onInputChanged(text: String): List<AwesomeBar.Suggestion> {
         if (text.isNullOrBlank()) {

commit 430b9ca7b7e4b89d90ac241f20d07c003e3324e1

Fix suggestions flickering caused by #12444

components/compose/awesomebar/src/main/java/mozilla/components/compose/browser/awesomebar/AwesomeBar.kt
@@ -40,24 +40,15 @@ fun AwesomeBar(
     onScroll: () -> Unit = {},
     profiler: Profiler? = null
 ) {
-    val mapTitlesToProviders = HashMap<String?, ArrayList<AwesomeBar.SuggestionProvider>>()
-    for (provider in providers) {
-        var groupedProviders: ArrayList<AwesomeBar.SuggestionProvider>? =
-            mapTitlesToProviders[provider.groupTitle()]
-        if (groupedProviders == null) {
-            groupedProviders = ArrayList()
-        }
-        groupedProviders.add(provider)
-        mapTitlesToProviders[provider.groupTitle()] = groupedProviders
-    }
-    val groups: ArrayList<AwesomeBar.SuggestionProviderGroup> = ArrayList()
-    for (item in mapTitlesToProviders) {
-        groups.add(
-            AwesomeBar.SuggestionProviderGroup(
-                providers = item.value,
-                title = item.key
-            )
-        )
+    val groups = remember(providers) {
+        providers
+            .groupBy { it.groupTitle() }
+            .map {
+                AwesomeBar.SuggestionProviderGroup(
+                    providers = it.value,
+                    title = it.key
+                )
+            }
     }
 
     AwesomeBar(
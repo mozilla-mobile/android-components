392ed3d6c5d15562f09ea7598142e95fd2da6526
Jan-Erik Rediger <jrediger@mozilla.com>
2022-08-01 18:51:27 +0200

    Bump to app-services v93.7.2, Glean v50.1.4

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -30,9 +30,9 @@ object Versions {
     const val disklrucache = "2.0.2"
     const val leakcanary = "2.8.1"
 
-    const val mozilla_appservices = "93.7.1"
+    const val mozilla_appservices = "93.7.2"
 
-    const val mozilla_glean = "50.1.3"
+    const val mozilla_glean = "50.1.4"
 
     const val material = "1.2.1"
 

b12a3072aace63146551d25ce215d1d7743819c4
Donal Meehan <76789979+DonalMe@users.noreply.github.com>
2022-10-29 13:28:31 -0400

    Revert "Update GeckoView (Beta) to 107.0.20221029133721." (#13033)
    This reverts commit 7347bb22b31ad9c52543199a5aa37748cc33e517.

--- buildSrc/src/main/java/Gecko.kt
+++ buildSrc/src/main/java/Gecko.kt
@@ -9,7 +9,7 @@ object Gecko {
     /**
      * GeckoView Version.
      */
-    const val version = "107.0.20221029133721"
+    const val version = "107.0.20221027185833"
 
     /**
      * GeckoView channel
